ca54ac5e4fe3ccb265c3485827f8fc8d5fac2c84
Paul Bovis <paul.bovis@canada.ca>
2018-04-05 13:43:08 -0600

    Vue can read in translations in Laravel.

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">{{ trans('app.general.appName') }}</div>
 
                     <div class="panel-body">
                         I'm an example component!
@@ -17,7 +17,7 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            console.log(this.trans('app.general.appName'))
         }
     }
 </script>
