bcf69cce2d995c304923ef80aaf3e26bbfc3f2ca
Bovis <Paul.Bovis2@ec.gc.ca>
2018-10-31 13:53:48 -0600

    Test the addition of APP_ENV environment variable to api calls

--- app/Http/Users/Controllers/Api/UsersController.php
+++ app/Http/Users/Controllers/Api/UsersController.php
@@ -29,7 +29,7 @@ class UsersController extends Controller
                 ],
                 'actionButton' => [
                     'active' => true,
-                    'endpoint' => '/users/',
+                    'endpoint' => env('APP_URL') . '/users/',
                     'endpointSuffix' => '',
                     'text' => 'Profile'
                 ]
